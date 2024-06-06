55042a0216160ea7e4eeafd9c3b266e1bfe336aa
marcos.pinho <marcos.pinho@thebrooklynbrothers.com>
2024-06-06 10:55:49 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import ImageIcon from 'part:@sanity/base/image-icon'
 import AssetBrowser from './app'
 
 export default {
-  name: 'media',
+  name: 'media-test',
   title: 'Media',
   component: AssetBrowser,
   icon: ImageIcon
